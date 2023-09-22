448714317023443fc0daa5c203a0b1aa20f22c45
egokick <egokick@gmail.com>
2023-09-22 15:03:31 -0700

    Python install for windows
    Download and installs python 3.10.6 if python 3.9 or greater is not found

--- windows-install.bat
+++ windows-install.bat
@@ -1,30 +1,94 @@
 @echo off
+SETLOCAL
+
 REM Check if Python is installed and in PATH
 where python >nul 2>nul
 if %errorlevel% neq 0 (
     echo Python is not installed or not in PATH.
+    goto InstallPython
+)
+
+REM Check Python version if installed
+for /f "tokens=*" %%i in ('python --version 2^>^&1') do set PYTHON_VERSION=%%i
+
+REM Check if the version string actually contains "Python"
+echo %PYTHON_VERSION% | find "Python" > nul
+if %errorlevel% neq 0 (
+    echo Python is not installed or not you do not have python > 3.9 in PATH.
+    goto InstallPython
+)
+
+set PYTHON_VERSION=%PYTHON_VERSION:~7%
+for /f "tokens=1,2,3 delims=." %%a in ("%PYTHON_VERSION%") do (
+    set Major=%%a
+    set Minor=%%b
+    set Patch=%%c
+)
+echo Detected Python version: %Major%.%Minor%.%Patch%
+
+REM Perform numerical comparison to check if version is adequate
+if %Major% geq 3 (
+    if %Major% gtr 3 (
+        echo Python version is adequate.
+        goto End
+    ) else (
+        if %Minor% geq 9 (
+            echo Python version is adequate.	    
+            goto End
+        )
+    )
+)
+
+echo Python version is not adequate.
+goto InstallPython
+
+:InstallPython
+echo Installing Python 3.10.6...
+REM Note: Internet access is required for this part, and it's disabled in this session. Make sure you're connected.
+REM Check if Python installer already exists
+if not exist "%CD%\python-3.10.6-amd64.exe" (
+    REM Download Python 3.10.6 using curl.
+    curl -O https://www.python.org/ftp/python/3.10.6/python-3.10.6-amd64.exe
+)
+start /wait python-3.10.6-amd64.exe InstallAllUsers=1 PrependPath=1
+if %errorlevel% neq 0 (
+    echo Failed to install Python.
     exit /b 1
+)
+echo Python has been installed, run the install script again.
+echo This will refresh the environment variables, ensure all other command windows are closed.
+pause
+
+
+:End
+ENDLOCAL
+
+echo Upgrade pip
+python -m pip install --upgrade pip
+if %errorlevel% neq 0 (
+    echo Failed to upgrade pip
+    pause
 )
 
 REM Install Python dependencies from requirements.txt
 pip install -r ./install/requirements.txt
 if %errorlevel% neq 0 (
     echo Failed to install Python dependencies from requirements.txt.
-    exit /b 1
+    pause
 )
 
 REM Check if Poetry is installed and in PATH
 where poetry >nul 2>nul
 if %errorlevel% neq 0 (
     echo Poetry is not installed or not in PATH.
-    exit /b 1
+    pause
 )
 
 REM Run 'poetry install' to install dependencies via Poetry
 poetry install
 if %errorlevel% neq 0 (
     echo Failed to install Python dependencies via Poetry.
-    exit /b 1
+    pause
 )
 
 REM Use local build folder
@@ -37,6 +101,7 @@ python ./install/windows-install.py
 REM Check the exit code of the Python script
 if %errorlevel% neq 0 (
     echo Failed to execute Python script.
-    exit /b 1
+    pause
 )
-echo Successfully executed Python script.
+echo Success Installed.
+pause
--- windows-uninstall.bat
+++ windows-uninstall.bat
@@ -2,7 +2,8 @@
 setlocal enabledelayedexpansion
 
 :: Display warning message
-echo This uninstalls *** ALL *** python libraries so you can try the install script again.
+echo WARNING this uninstalls *** ALL *** python libraries
+echo WARNING this deletes the poetry.lock file
 echo Are you sure you want to continue? y/N
 set /p user_input=
 
@@ -15,19 +16,33 @@ if /i "%user_input%"=="y" (
     for %%A in (installed_packages.txt) do (
         if %%~zA==0 (
             echo all pip packages uninstalled
-            goto cleanup
+            goto poetry_check
         )
     )
     
     pip uninstall -r installed_packages.txt -y
+    goto poetry_check
 ) else (
     echo Exiting...
     goto end
 )
 
+:poetry_check
+:: Check if poetry is installed
+poetry -V >nul 2>&1
+if %ERRORLEVEL% equ 0 (
+    echo Poetry is installed, removing all environments...
+    for /f "delims=" %%i in ('poetry env list') do poetry env remove %%i
+) else (
+    echo Poetry is not installed, skipping poetry environment removal.
+)
+
 :cleanup
 :: Clean up
 if exist installed_packages.txt del installed_packages.txt
+
+:: Delete poetry.lock if it exists
+if exist poetry.lock del poetry.lock
 
 :end
 :: End of script
