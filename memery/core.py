f6a2c98016d2c7254a46dd09ece934668d1fc310
deepfates <deepfates@gmail.com>
2024-10-14 16:42:18 -0700

    feat: add mps backend

--- memery/core.py
+++ memery/core.py
@@ -21,7 +21,7 @@ class Memery():
         self.index = None
         self.db = None
         self.model = None
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
         print(f"Using {self.device} for computation.")
 
     def index_flow(self, root: str, num_workers=0) -> tuple[str, str]:
